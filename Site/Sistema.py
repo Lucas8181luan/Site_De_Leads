4e9cae4b1c0b25ebb9bc0ce3d7fc271aefedb3b5
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2025-02-21 12:48:37 -0300

    Todos inputs estão feitos

--- Site/Sistema.py
+++ Site/Sistema.py
@@ -30,19 +30,76 @@ def home():
             border-radius: 10px;
             padding-left: 20px;
         }
-            input[id="confrimação E-mail"] {
+        input[id="confrimação E-mail"] {
             height: 50px;
             width: 500px;
             font-size: 15px;
             border-radius: 10px;
             padding-left: 20px;
         }
-            input[id="telefone"] {
+        input[id="telefone"] {
             height: 50px;
             width: 500px;
             font-size: 15px;
             border-radius: 10px;
             padding-left: 20px;
+        }
+        input[id="data de nascimento"] {
+            height: 50px;
+            width: 500px;
+            font-size: 15px;
+            border-radius: 10px;
+            padding-left: 20px;
+        }
+        select[id="curso escolhido"] {
+            height: 50px;
+            width: 525px;
+            font-size: 15px;
+            border-radius: 10px;
+            padding-left: 20px;
+        }
+        select[id="horário de estudo"] {
+            height: 50px;
+            width: 525px;
+            font-size: 15px;
+            border-radius: 10px;
+            padding-left: 20px;
+        }
+        input[id="cep"] {
+            height: 50px;
+            width: 500px;
+            font-size: 15px;
+            border-radius: 10px;
+            padding-left: 20px;
+        }
+        select[id="bairro"] {
+            height: 50px;
+            width: 525px;
+            font-size: 15px;
+            border-radius: 10px;
+            padding-left: 20px;
+        }
+        input[id="cpf"] {
+            height: 50px;
+            width: 500px;
+            font-size: 15px;
+            border-radius: 10px;
+            padding-left: 20px;
+        }
+        select[id="sexo"] {
+            height: 50px;
+            width: 525px;
+            font-size: 15px;
+            border-radius: 10px;
+            padding-left: 20px;
+        }
+        button[id="enviar"] {
+            height: 50px;
+            width: 200px;
+            font-size: 25px;
+            border-radius: 10px;
+            background-color: blue;
+            color: white;
         }
     </style>
     <body>
@@ -54,7 +111,48 @@ def home():
             <br><p>
             <input type="text" id="confrimação E-mail" placeholder="CONFIRMAÇÃO E-MAIL">
             <br><p>
-            <input type="number" id="telefone" placeholder="TELEFONE (xx)xxxxx-xxxx">
+            <input type="number" id="telefone" placeholder="TELEFONE (DDD)xxxxx-xxxx">
+            <br><p>
+            <input type="date" id="data de nascimento" placeholder="DATA DE NASCIMENTO (+18)">
+            <br><p>
+            <select id="curso escolhido">
+                <option value=".">ESCOLHA SEU CURSO</option>
+                <option value="EAD - AGENTE DE DEFESA AMBIENTAL">EAD - AGENTE DE DEFESA AMBIENTAL</option>
+                <option value="HÍBRIDO - ATENDENTE DE FARMÁCIA">HÍBRIDO - ATENDENTE DE FARMÁCIA</option> 
+                <option value="HÍBRIDO - AUXILIAR ADMINISTRATIVO">HÍBRIDO - AUXILIAR ADMINISTRATIVO</option> 
+                <option value="HÍBRIDO - OPERADOR DE CAIXA">HÍBRIDO - OPERADOR DE CAIXA</option> 
+                <option value="HÍBRIDO - DESIGNER DE SOMBRANCELHAS">HÍBRIDO - DESIGNER DE SOMBRANCELHAS</option> 
+                <option value="HÍBRIDO - CUMIN">HÍBRIDO - CUMIN</option>
+                <option value="HÍBRIDO - GARÇOM">HÍBRIDO - GARÇOM</option> 
+                <option value="PRESENCIAL - INFORMÁTICA">PRESENCIAL - INFORMÁTICA</option> 
+                <option value="PRESENCIAL - CUIDADOR DE IDOSO">PRESENCIAL - CUIDADOR DE IDOSO</option> 
+            </select>
+            <br><p>
+            <select id="horário de estudo">
+                <option value=".">ESCOLHA SEU HORÁRIO DE ESTUDO</option>
+                <option value="MANHÃ">MANHÃ</option>
+                <option value="TARDE">TARDE</option>]
+                <option value="NOITE">NOITE</option>
+            </select>
+            <br><p>
+            <input type="number" id="cep" placeholder="CEP">
+            <br><p>
+            <select id="bairro">
+                <option value=".">LOCAL DE ESTUDO</option>
+                <option value="PENHA - CASA NAILS BRASIL">PENHA - CASA NAILS BRASIL</option>
+                <option value="MADUREIRA - IFP">MADUREIRA - IFP</option>]
+                <option value="CENTRO - EMPREENDA.RIO">CENTRO - EMPREENDA.RIO</option>
+            </select>
+            <br><p>
+            <input type="number" id="cpf" placeholder="CPF">
+            <br><p>
+            <select id="sexo">
+                <option value=".">SEXO</option>
+                <option value="M">MASCULINO</option>
+                <option value="F">FEMININO</option>]
+            </select>
+            <br><br><p>
+            <button type"submit" id="enviar"><strong>ENVIAR</strong></button>
         </div>
     </body>
     </html>
